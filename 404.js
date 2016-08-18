35e0176173b7445644e35ac5fd6d9a782910cae2
TestThouv <thouvgh1@outlook.fr>
2016-08-18 17:06:41 -0400

    Create 404.js

---
+++ 404.js
@@ -0,0 +1,12 @@
+var myName = "404";
+
+var white = [0, 0, 100];
+var black = [0, 0, 27];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [white, black];
+
+drawName(myName, letterColors);
+
+bounceBubbles();
