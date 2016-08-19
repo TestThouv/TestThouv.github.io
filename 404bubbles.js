20cfe4b91b9a02312097a0580026ac281a5a5aa3
TestThouv <thouvgh1@outlook.fr>
2016-08-18 21:03:35 -0400

    Update 404bubbles.js

--- 404bubbles.js
+++ 404bubbles.js
@@ -5,7 +5,7 @@ var black = [0, 0, 27];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var letterColors = [black, white];
+var letterColors = [white, black];
 
 drawName(myName, letterColors);
 
