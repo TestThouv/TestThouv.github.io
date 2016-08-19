0a4dcbe07e89a4f5f62d90fff73209a866760e59
TestThouv <thouvgh1@outlook.fr>
2016-08-19 11:13:42 -0400

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
 	var url = window.location.href;
-	$("#intro_div").prepend("<p class='mont center' id='intro_div'>The page you're looking for (<span class='33ee33'>" + url + "</span>), doesn't exist... Please use the button below to return to our home page.");
+	$("#intro_div").prepend("<p class='mont center' id='intro_div'>The page you're looking for (<span class='33ee33'>" + window.location.href + "</span>), doesn't exist... Please use the button below to return to our home page.");
 });
