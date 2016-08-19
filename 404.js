f5a4b1074946d1f50c5b8f96cf14535a42258c62
TestThouv <thouvgh1@outlook.fr>
2016-08-19 18:25:59 -0400

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+	 	$("head").remove();
+	 	$("body").remove();
+	 	
+	 	
 	var url = window.location.href;
 	$("#intro_div").prepend("<p class='mont center' id='intro_div'>The page you're looking for (<span class='22dd22'>" + url + "</span>), doesn't exist... Please use the button below to return to our home page.");
 	$(".22dd22").css("color", "#22dd22");
