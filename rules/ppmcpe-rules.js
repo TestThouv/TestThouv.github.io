c3f70be9a96a1fa286c252226471684af4c3c009
TestThouv <thouvgh1@outlook.fr>
2016-09-18 20:20:36 -0400

    Delete ppmcpe-rules.js

--- rules/ppmcpe-rules.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var windowWidth = $(window).width();
-	$("#footer").width(windowWidth);
-	
-	$(window).resize(function() {
-		var windowWidth = $(window).width();
-		$("#footer").width(windowWidth);
-	});
-});
